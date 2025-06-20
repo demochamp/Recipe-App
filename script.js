a9712b82e47adcdbed5daa7edf3dff3ec38a5325
demochamp <demochamp05@gmail.com>
2025-06-20 22:59:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ searchBtn.addEventListener('click',(e)=>{
     fetchRecipes(searchInput);
 });
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js')
-    .then(reg => console.log('Service Worker registered', reg))
+  navigator.serviceWorker.register('/Recipe-App/service-worker.js')
+.then(reg => console.log('Service Worker registered', reg))
     .catch(err => console.error('Service Worker registration failed', err));
 }
