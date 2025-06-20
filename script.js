5ac3f89039cecbd7c8af712c7b98df6c2eaf1b71
demochamp <demochamp05@gmail.com>
2025-06-20 21:46:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -81,3 +81,8 @@ searchBtn.addEventListener('click',(e)=>{
     }
     fetchRecipes(searchInput);
 });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js')
+    .then(reg => console.log('Service Worker registered', reg))
+    .catch(err => console.error('Service Worker registration failed', err));
+}
