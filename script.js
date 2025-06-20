ab25f22527bde24423636cbe85418a752b08ffed
demochamp <demochamp05@gmail.com>
2025-06-20 23:16:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-const searchBox =document.querySelector('.searchBox');
-const searchBtn =document.querySelector('.searchBtn');
-const recipeContainer =document.querySelector('.recipe-container');
-const recipeDetailsContent =document.querySelector('.recipe-details-content');
-const recipeCloseBtn =document.querySelector('.recipe-close-Btn');
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+const recipeDetailsContent = document.querySelector('.recipe-details-content');
+const recipeCloseBtn = document.querySelector('.recipe-close-Btn');
 
 // Function to get recipes
 const fetchRecipes = async (query) => {
     recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>";
     try {
-        
-    
-    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
-    const response = await data.json();
 
-    recipeContainer.innerHTML = "";
-    response.meals.forEach(meal => {
-         const recipeDiv = document.createElement('div');
-         recipeDiv.classList.add('recipe');
-         recipeDiv.innerHTML = `
+
+        const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+        const response = await data.json();
+
+        recipeContainer.innerHTML = "";
+        response.meals.forEach(meal => {
+            const recipeDiv = document.createElement('div');
+            recipeDiv.classList.add('recipe');
+            recipeDiv.innerHTML = `
          <img src="${meal.strMealThumb}">
          <h3>${meal.strMeal}</h3>
          <p><span>${meal.strArea}</span> Dish</p>
          <p>Belongs to <span>${meal.strCategory}</span> Category</p>
          `
-         const button = document.createElement('button');
+            const button = document.createElement('button');
             button.textContent = "View Recipe";
             recipeDiv.appendChild(button);
-           
+
             // Adding EventListener to recipe button
             button.addEventListener('click', () => {
                 openRecipePopup(meal);
             });
-         recipeContainer.appendChild(recipeDiv);
-    });
+            recipeContainer.appendChild(recipeDiv);
+        });
     }
     catch (error) {
         recipeContainer.innerHTML = "<h2>Error in Fetching Recipes...</h2>";
     }
-    
+
 }
 
 // Function to fetch ingredients and measurements
@@ -45,7 +45,7 @@ const fetchIngredients = (meal) => {
     let ingredientsList = "";
     for (let i = 1; i <= 20; i++) {
         const ingredient = meal[`strIngredient${i}`];
-        if (ingredient){
+        if (ingredient) {
             const measure = meal[`strMeasure${i}`];
             ingredientsList += `<li>${measure} ${ingredient}</li>`
         }
@@ -65,14 +65,14 @@ const openRecipePopup = (meal) => {
             <p>${meal.strInstructions}</p>
         </div>
     `
-        
+
     recipeDetailsContent.parentElement.style.display = "block";
 }
 
 recipeCloseBtn.addEventListener('click', () => {
     recipeDetailsContent.parentElement.style.display = "none";
 });
-searchBtn.addEventListener('click',(e)=>{
+searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const searchInput = searchBox.value.trim();
     if (!searchInput) {
@@ -82,7 +82,7 @@ searchBtn.addEventListener('click',(e)=>{
     fetchRecipes(searchInput);
 });
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/Recipe-App/service-worker.js')
-.then(reg => console.log('Service Worker registered', reg))
-    .catch(err => console.error('Service Worker registration failed', err));
+    navigator.serviceWorker.register('/Recipe-App/service-worker.js', { scope: '/Recipe-App/' })
+        .then(reg => console.log('Service Worker registered', reg))
+        .catch(err => console.error('Service Worker registration failed', err));
 }
